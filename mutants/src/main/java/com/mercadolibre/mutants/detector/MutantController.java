30336eb41878781361edbc7320c47d2c98b3c8f3
Enrique Lamberto <enquibe@gmail.com>
2021-05-09 07:09:05 +0000

    Agrego (temporalmente) ruta para consultar propiedad mongodb.uri

--- mutants/src/main/java/com/mercadolibre/mutants/detector/MutantController.java
+++ mutants/src/main/java/com/mercadolibre/mutants/detector/MutantController.java
@@ -1,8 +1,11 @@
 package com.mercadolibre.mutants.detector;
 
 import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +17,16 @@ public class MutantController {
 
     private final MutantService mutantService;
 
+    @Value("${spring.data.mongodb.uri:1234}")
+    private String mongodbUri;
+
     public MutantController(MutantService mutantService) {
         this.mutantService = mutantService;
+    }
+
+    @GetMapping()
+    public String getMongoUri() {
+        return mongodbUri;
     }
 
     @PostMapping()
