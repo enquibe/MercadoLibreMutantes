400cf46a4d92b63f835750acfb021c47f8be2fa8
Enrique Lamberto <enquibe@gmail.com>
2021-05-09 18:35:50 +0000

    Actualizo DnaRepositorytests

--- mutants/src/test/java/com/mercadolibre/mutants/DnaRepositoryTests.java
+++ mutants/src/test/java/com/mercadolibre/mutants/DnaRepositoryTests.java
@@ -1,13 +1,66 @@
 package com.mercadolibre.mutants;
 
-import com.mercadolibre.mutants.db.DnaRepository;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Optional;
+import com.mercadolibre.mutants.model.Dna;
+import com.mercadolibre.mutants.repository.DnaRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 
 @DataMongoTest
 public class DnaRepositoryTests {
     
-    private final DnaRepository dnaRepository;
+    @Autowired
+    private DnaRepository repository;
 
-    
+    @BeforeEach
+	void clean() {
+		repository.deleteAll();
+	}
+
+    @Test
+    void injectedComponentIsNotNull(){
+        assertNotNull(repository);
+    }
+
+    @Test
+	void setsIdOnSave() {
+        String[] mutantDna = { "ATGCGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCTA", "TCACTG" };
+		var savedDna = repository.save(new Dna(mutantDna, true));
+        assertNotNull(savedDna.getId());
+	}
+
+    @Test
+    void findsDnaBySequence() {
+        String[] mutantDna = { "ATGCGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCTA", "TCACTG" };
+        var savedDna = repository.save(new Dna(mutantDna, true));
+
+        Optional<Dna> foundDna = repository.findBySequence(mutantDna);
+
+        assertTrue(foundDna.isPresent());
+        assertEquals(savedDna, foundDna.get());
+    }
+
+    @Test
+    void countsByIsMutant() {
+        String[] mutantDna = { "ATGCGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCTA", "TCACTG" };
+        repository.save(new Dna(mutantDna, true));
+
+        String[] humanDna = { "ATGCGA", "CGGTGC", "TTATGT", "AGAAGG", "CCCCTA", "TCACTG" };
+        repository.save(new Dna(humanDna, false));
+
+        String[] humanDna2 = { "ATGCGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCTA", "TCACTG" };
+        repository.save(new Dna(humanDna2, false));
+
+        long mutantCount = repository.countByIsMutant(true);
+        long humanCount = repository.countByIsMutant(false);
+
+        assertEquals(mutantCount, 1);
+        assertEquals(humanCount, 2);
+    }
 }
