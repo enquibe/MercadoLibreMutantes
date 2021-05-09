6a06db5b99efc7593dba0e8cbc7314d668695d08
Enrique Lamberto <enquibe@gmail.com>
2021-05-09 07:13:56 +0000

    Revert "Agrego (temporalmente) ruta para consultar propiedad mongodb.uri"
    This reverts commit 30336eb41878781361edbc7320c47d2c98b3c8f3.

--- mutants/src/main/java/com/mercadolibre/mutants/detector/MutantController.java
+++ mutants/src/main/java/com/mercadolibre/mutants/detector/MutantController.java
@@ -1,11 +1,8 @@
 package com.mercadolibre.mutants.detector;
 
 import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,16 +14,8 @@ public class MutantController {
 
     private final MutantService mutantService;
 
-    @Value("${spring.data.mongodb.uri:1234}")
-    private String mongodbUri;
-
     public MutantController(MutantService mutantService) {
         this.mutantService = mutantService;
-    }
-
-    @GetMapping()
-    public String getMongoUri() {
-        return mongodbUri;
     }
 
     @PostMapping()
